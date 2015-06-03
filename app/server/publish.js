a8dd2d7c5cdae0059b6db353b7cd8bc1dfc053b7
ianngiaw <ian@u.nus.edu>
2015-06-04 01:20:36 +0800

    Added allUserData and eventData to publish

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,7 +1,11 @@
-Meteor.publish("userData", function () {
+Meteor.publish("allUserData", function () {
 	return Meteor.users.find();
 });
 
 Meteor.publish("subData", function () {
 	return Subscriptions.find({subberId: this.userId});
+});
+
+Meteor.publish("eventData", function() {
+	return Events.find();
 });
