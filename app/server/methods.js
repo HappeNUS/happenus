7bdd9c080414ba731e650ddd8314a4ecc7e9ce39
ianngiaw <ian@u.nus.edu>
2015-06-04 01:21:34 +0800

    Adjusted behavior of subscribing to a user. Only can subscribe once.

--- app/server/methods.js
+++ app/server/methods.js
@@ -1,11 +1,17 @@
 Meteor.methods({
 	'sub': function(otherUser) {
 		if (this.userId !== otherUser) {
-			Subscriptions.insert({
+			var subscription = Subscriptions.findOne({
 				subberId: this.userId,
-				subbedId: otherUser,
-				date: new Date()
+				subbedId: otherUser
 			});
+			if (!subscription) {
+				Subscriptions.insert({
+					subberId: this.userId,
+					subbedId: otherUser,
+					date: new Date()
+				});
+			}
 		}
 	},
 	'unsub': function(subbedUser) {
