3d93caa0fc0c8409fe8a610592e628b0f509557e
ianngiaw <ian@u.nus.edu>
2015-06-04 17:00:33 +0800

    Added functionality to subscriptions template to show events created by subscribed users

--- app/client/templates/views/subs/subscriptions/subscriptions.js
+++ app/client/templates/views/subs/subscriptions/subscriptions.js
@@ -1,18 +1,10 @@
 Template.subscriptions.onCreated(function(){
+	this.subscribe("subEventData");
 	this.subscribe("subData");
 });
 
 Template.subscriptions.helpers({
-	subbed: function() {
-		return Subscriptions.find();
-	}
-});
-
-Template.subscriptions.events({
-	'click .add-btn': function(event){
-		Meteor.call('sub', Fake.word());
-	},
-	'click .rem-btn': function(event){
-		Meteor.call('unsub', this.subbedId);
+	subEvents: function() {
+		return Events.find();
 	}
 });
