d6465bb011b8d8f491ca91aa8d01f267807494f1
ianngiaw <ian@u.nus.edu>
2015-06-04 16:58:48 +0800

    Published more data. userEventData for finding all events by a specific user, subEventData for finding all subscribed events and specificEventData for finding event data of one specific event

--- app/server/publish.js
+++ app/server/publish.js
@@ -3,9 +3,25 @@ Meteor.publish("allUserData", function () {
 });
 
 Meteor.publish("subData", function () {
-	return Subscriptions.find({subberId: this.userId});
+	return Subs.find({subberId: this.userId});
 });
 
 Meteor.publish("eventData", function() {
 	return Events.find();
+});
+
+Meteor.publish("userEventData", function(userId) {
+	return Events.find({userId: userId});
+});
+
+Meteor.publish("subEventData", function(){
+	var subscriptions = Subs.find({subberId: this.userId}).fetch();
+	var subbedIds = subscriptions.map(function(curr, idx, arr) {
+		return curr.subbedId;
+	});
+	return Events.find({userId: {$in: subbedIds}});
+});
+
+Meteor.publish("specificEventData", function(eventId){
+	return Events.find({_id: eventId});
 });
