a186c98943dab3f9760050ac13371e6db10a16ce
ianngiaw <ian@u.nus.edu>
2015-06-04 17:58:55 +0800

    Updated event page copied from a template http://startbootstrap.com/template-overviews/portfolio-item/

--- app/client/templates/views/event/event.js
+++ app/client/templates/views/event/event.js
@@ -1,9 +1,18 @@
+function getCurrentEvent() {
+	return Events.findOne({_id: Router.current().params._id});
+}
+
 Template.event.onCreated(function(){
-	this.subscribe("specificEventData", Router.current().params._id);
+	var userId = getCurrentEvent().userId;
+	this.subscribe("specificUserData", userId);
 });
 
 Template.event.helpers({
 	currentEvent: function(){
-		return Events.findOne({_id: Router.current().params._id});
+		return getCurrentEvent();
+	},
+	userData: function(){
+		var curr = getCurrentEvent();
+		return Meteor.users.findOne({_id: curr.userId});
 	}
 });
