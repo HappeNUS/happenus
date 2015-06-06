886fe0c5e2b6b54243eb584b07f248885d02550d
unknown <ianngiawtingan@gmail.com>
2015-06-06 20:41:51 +0800

    Added button at bottom of eventcard to like/unlike content. Edited hover transitions.. Made entire card clickable.

--- app/client/templates/common/eventcard/eventcard.js
+++ app/client/templates/common/eventcard/eventcard.js
@@ -2,8 +2,28 @@ Template.eventcard.onCreated(function(){
 	this.subscribe("allUserData", this.userId);
 });
 
+function isEventLiked (likes) {
+	return likes.indexOf(Meteor.userId()) !== -1;
+}
+
 Template.eventcard.helpers({
 	user: function(){
 		return Meteor.users.findOne({_id: this.userId});
+	},
+	likeCount: function(){
+		return this.likeCount;
+	},
+	isEventLiked: function(){
+		return isEventLiked(this.likes);
+	}
+});
+
+Template.eventcard.events({
+	'click .like-btn': function(event, template) {
+		if (isEventLiked(this.likes)) {
+			Meteor.call('unlikeEvent', this._id);
+		} else {
+			Meteor.call('likeEvent', this._id);
+		}
 	}
 });
