ea5512791a07ba99df369dc2293587c57db7d912
ianngiaw <ian@u.nus.edu>
2015-06-08 00:27:14 +0800

    Modified eventcard to show tags on the top right hand corner of each card. Behaviour: 0 not shown, 1 shown, >1 shown every 4 sec

--- app/client/templates/common/eventcard/eventcard.js
+++ app/client/templates/common/eventcard/eventcard.js
@@ -2,6 +2,26 @@ Template.eventcard.onCreated(function(){
 	this.subscribe("allUserData", this.userId);
 });
 
+Template.eventcard.onRendered(function(){
+	if (this.data.tags.length > 0) {
+		var counter = 0;
+		var template = this;
+		var data = this.data;
+		var box = template.$(".card-tag-box");
+		box.text(data.tags.slice(-1)[0]);
+		if (data.tags.length > 1) {
+			window.setInterval(change, 5000);
+		}
+	}
+	function change() {
+			box.fadeOut(500, function() {
+				box.text(data.tags[counter++]);
+				box.fadeIn(500);
+			});
+			counter = counter % data.tags.length;
+		}
+});
+
 function isEventLiked (likes) {
 	return likes.indexOf(Meteor.userId()) !== -1;
 }
@@ -15,6 +35,9 @@ Template.eventcard.helpers({
 	},
 	isEventLiked: function(){
 		return isEventLiked(this.likes);
+	},
+	hasTags: function() {
+		return this.tags.length !== 0;
 	}
 });
 
