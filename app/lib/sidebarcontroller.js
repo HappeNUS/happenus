8dd97bb0ad59f7d5e6ec6cb515dc40719e2b6603
ianngiaw <ian@u.nus.edu>
2015-06-11 01:58:52 +0800

    Updated main layout to materialize

--- app/lib/sidebarcontroller.js
+++
@@ -1,9 +0,0 @@
-SidebarController = {
-	selectSidebar: function (selector) {
-		this.clearSidebar();
-		$(selector).addClass('active');
-	},
-	clearSidebar: function  () {
-		$('.nav-sidebar').children().removeClass('active');
-	}
-};
