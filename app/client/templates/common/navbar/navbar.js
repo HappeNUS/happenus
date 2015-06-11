9db93dc40c1e3309dbb10861f5d238155b521f41
ianngiaw <ian@u.nus.edu>
2015-06-11 17:46:44 +0800

    Edited navbar to look better

--- app/client/templates/common/navbar/navbar.js
+++ app/client/templates/common/navbar/navbar.js
@@ -1,3 +1,9 @@
 Template.navbar.onRendered(function () {
 	$('.button-collapse').sideNav();
 });
+
+Template.navbar.events({
+	'click a': function (event, temp) {
+		$('.button-collapse').sideNav('hide');
+	}
+});
