47f0b384a682fbc0a3b6d7e1bfad48718d44616f
ianngiaw <ian@u.nus.edu>
2015-06-17 16:20:03 +0800

    Options now have different positions based on scroll position

--- app/client/templates/views/home/home.js
+++ app/client/templates/views/home/home.js
@@ -15,6 +15,22 @@ Template.home.onCreated(function () {
 	});
 });
 
+Template.home.onRendered(function(){
+	var navbar = $('nav div.nav-wrapper');
+	var tabs = $('.home .homeTabs');
+	$(window).scroll(function(event){
+		var scrollTop = $(this).scrollTop();
+		var options = $('#options-non-mobile');
+		if (scrollTop > tabs.height()) {
+			options.addClass('pin-options');
+			options.css('top', navbar.height() + 18);
+		} else {
+			options.removeClass('pin-options');
+			options.css('top', 'initial');
+		}
+	});
+});
+
 Template.home.helpers({
 	events: function() {
 		// All events to be shown on the page
