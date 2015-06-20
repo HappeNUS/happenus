a0ae51efdf36dce01a3d0bc875e726ee9e16192b
unknown <ianngiawtingan@gmail.com>
2015-06-20 22:48:59 +0800

    Created notificationfactory methods for mass notifications and deleting event notifications fixes #3

--- app/lib/notificationfactory.js
+++ app/lib/notificationfactory.js
@@ -1,5 +1,18 @@
 NotificationFactory = {
 	newNotification: function(targetUserId, title, description, link){
 		return Notifications.new({owner: targetUserId, class: title, title: description, link: link});
+	},
+	massNotification: function(userIds, title, description) {
+		for (var i = 0; i < userIds.length; i++) {
+			if (Meteor.userId() !== userIds[i]) {
+				Notifications.new({owner: userIds[i], class: title, title: description});
+			}
+		}
+	},
+	eventDelNotif: function(event) {
+		var user = Meteor.users.findOne({_id: event.userId});
+		var title = "An event you liked, '" + event.name + "', has been deleted";
+		var description = "Deleted by " + user.username;
+		this.massNotification(event.likes, title, description);
 	}
 };
