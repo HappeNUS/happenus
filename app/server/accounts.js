0d53316b087aef6f41fd4e3018d57e0f8b842a19
ianngiaw <ian@u.nus.edu>
2015-06-27 00:46:44 +0800

    user gets a default profile img on create fixes #4

--- app/server/accounts.js
+++ app/server/accounts.js
@@ -1,9 +1,4 @@
 Accounts.onCreateUser(function(options, user) {
+	user.profile_img = Meteor.settings.Cloudinary.default_profile_img; 
 	return user;
-});
-
-Meteor.users.deny({
-	update: function() {
-		return true;
-	}
 });
