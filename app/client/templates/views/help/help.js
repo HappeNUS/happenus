d4556574b7b18a89c119fa7f5ca85609a568a41f
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-01 17:08:07 +0800

    Add animation when jump page

--- app/client/templates/views/help/help.js
+++ app/client/templates/views/help/help.js
@@ -1,9 +1,10 @@
 Template.help.onRendered(function () {
-	$('.button-collapse').sideNav();
-});
-
-Template.help.events({
-	'click a': function (event, temp) {
-		$('.button-collapse').sideNav('hide');
-	}
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	   $('html, body').stop().animate({
+	   	'scrollTop': $target.offset().top
+	   }, 200, 'swing');
+	});
 });
