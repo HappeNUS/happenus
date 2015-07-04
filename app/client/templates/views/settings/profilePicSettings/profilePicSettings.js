1c2984dcd27dc863ef3f04a02ace8d61a3f3ab83
ianngiaw <ian@u.nus.edu>
2015-07-04 18:26:06 +0800

    Completed profilepicsettings for changing profile picture

---
+++ app/client/templates/views/settings/profilePicSettings/profilePicSettings.js
@@ -0,0 +1,52 @@
+var PROFILE_RESIZE_OPTIONS = {
+	width: 150,
+	height: 150,
+	cropSquare: true
+};
+
+Template.profilePicSettings.onCreated(function(){
+	Session.set("img_preview", "");
+});
+
+Template.profilePicSettings.helpers({
+	img_preview: function(){
+		return Session.get("img_preview");
+	},
+	profile_img_url: function(){
+		return $.cloudinary.url(Meteor.user().profile.img);
+	}
+});
+
+Template.profilePicSettings.events({
+	'click .profilePicSettings div.file-input button': function (event, template) {
+		template.$('.profilePicSettings div.file-input input[type=file]').click();
+	},
+	'change .file-input input[type=file]': function (event, template) {
+		var files = event.target.files;
+		if (files.length === 1) {
+			var file = files[0];
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				Session.set("img_preview", e.target.result)
+			};
+
+			reader.readAsDataURL(file);
+		}
+	},
+	'click .img-upload button.upload-btn': function (event, template) {
+		var files = template.find('.file-input input[type=file]').files;
+		if (files) {
+			Resizer.resize(files[0], PROFILE_RESIZE_OPTIONS, function (err, file) {
+				if (!err) {
+					C.upload_stream([file], function(res) {
+						Session.set("img_preview", "");
+						Meteor.call('profileUpload', res);
+					});
+				} else {
+					console.log(err);
+				}
+			});
+		}
+	}
+});
