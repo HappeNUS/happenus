e262d61f9a19df54b19e5a0d181c8e90ed6530c4
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-05 03:45:37 +0800

    Click anywhere to close searchbar

--- app/client/templates/common/navbar/navbar.js
+++ app/client/templates/common/navbar/navbar.js
@@ -1,6 +1,18 @@
 Template.navbar.onRendered(function () {
 	$('.button-collapse').sideNav();
+
 	$('#search').hide();
+
+	$(document).mouseup(function(event){
+		var target1 = $(".form-control");
+		var target2 = $('#search');
+		var target3 =  $('#searchbar');
+    if(!(target1.is(event.target) || target2.is(event.target) || target3.is(event.target)) && target1.is(":visible")) {
+       target2.slideUp(100);
+    } else {
+      return false;
+    }
+  });
 });
 
 Template.navbar.events({
