ed39e3e75b4cd0ecf50571e7c9ab561e43c293d8
ianngiaw <ian@u.nus.edu>
2015-07-06 17:43:26 +0800

    Added eventLikeNotif and welcomeNotif

--- app/lib/notificationfactory.js
+++ app/lib/notificationfactory.js
@@ -32,6 +32,26 @@ if (Meteor.isServer) {
 					}
 				}
 			}
+		},
+		eventLikeNotif: function (eventId, likerId) {
+			var event = Events.findOne({_id: eventId});
+			var likeeId = event.userId;
+			if (likerId !== likeeId) {
+				var liker = Meteor.users.findOne({_id: likerId});
+				var likee = Meteor.users.findOne({_id: likeeId});
+
+				if (!likee.notifSettings || likee.notifSettings.liked) {
+					var title = liker.username + " liked your event '" + event.name + "'.";
+					var description = "at " + moment(new Date()).format('DD/MM/YYYY h:mm a');
+					var link = '/profile/' + likerId;
+					this.newNotification(likeeId, title, description, link);
+				}
+			}
+			
+		},
+		welcomeNotif: function(userId) {
+			var username = Meteor.users.findOne({_id: userId}).username;
+			this.newNotification(userId, 'Welcome to HappeNUS, ' + username + '!', 'Click on this notification to learn more about HappeNUS', '/help');
 		}
 	};
 }
