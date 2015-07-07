a6456faaec22186e07de291a66e09a857f3f2023
ianngiaw <ian@u.nus.edu>
2015-07-07 23:12:15 +0800

    Fixed bug where clicking on the label results in activating main add comment

--- app/client/templates/views/event/comments/addComment/addComment.js
+++ app/client/templates/views/event/comments/addComment/addComment.js
@@ -12,11 +12,20 @@ Template.addComment.onRendered(function() {
 	}
 	target.submit(function(event){
 		event.preventDefault();
-		var target = $(event.target).find('input#comment-input');
+		var inputSelector;
+		if (reply_id) {
+			inputSelector = 'input#comment-input-' + reply_id;
+		} else {
+			inputSelector = 'input#comment-input-main';
+		}
+		var target = $(event.target).find(inputSelector);
 		var text = target.val().trim();
 		if (text) {
 			Meteor.call('createComment', eventId, Meteor.userId(), text, reply_id);
 		}
 		target.val(null);
+		if (reply_id) {
+			reply.parents('.add-comment').slideUp(100);
+		}
 	});
 });
