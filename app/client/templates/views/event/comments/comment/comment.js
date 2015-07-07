011a43f466d761602731156dbeeed1b3d1b5953d
ianngiaw <ian@u.nus.edu>
2015-07-07 23:12:35 +0800

    Focus on reply automatically

--- app/client/templates/views/event/comments/comment/comment.js
+++ app/client/templates/views/event/comments/comment/comment.js
@@ -1,18 +1,23 @@
 Template.comment.onRendered(function () {
 	var margin = this.data.level * 5;
-	this.$('li.comment#' + this.data._id).css('margin-left', margin);
+	var _id = this.data._id;
+	var instance = this;
+	var complete = function () {
+		instance.$('li.comment#' + _id).find('input').focus();
+	}
+	this.$('li.comment#' + _id).css('margin-left', margin);
 
-	this.$('li.comment#' + this.data._id + ' .add-comment#add-' + this.data._id).hide();
-	this.$('#reply-' + this.data._id).click(function (event) {
+	this.$('li.comment#' + _id + ' .add-comment#add-' + _id).hide();
+	this.$('#reply-' + _id).click(function (event) {
 		var add = $(event.target).parents('li.comment').children('.add-comment');
 		if (add.is(':visible')) {
 			add.slideUp(100);
 		} else if ($('.add-comment').is(':visible')) {
 			$('.add-comment').slideUp(100, function(){
-				add.slideDown(100);				
+				add.slideDown(100, complete);				
 			});
 		} else {
-			add.slideDown(100);
+			add.slideDown(100, complete);
 		}
 	});
 });
