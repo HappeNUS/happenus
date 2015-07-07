d433ff013abacb985d899c0c3058b96eae4fbe6a
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-07 21:51:46 +0800

    A working share button

--- app/public/fbshare.js
+++ app/public/fbshare.js
@@ -1,5 +1,5 @@
 function popupwindow(w, h) {
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
-  return window.open("https://www.facebook.com/sharer/sharer.php?u=http://www.google.com", "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  return window.open("https://www.facebook.com/sharer/sharer.php?u="+ window.location.href, "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
   } 
