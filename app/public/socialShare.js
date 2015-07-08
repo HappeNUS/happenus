506035032f195da39b1cc31b88b5c4550f67822c
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-08 18:31:22 +0800

    Refactor

--- app/public/socialShare.js
+++ app/public/socialShare.js
@@ -1,17 +1,20 @@
 function popupwindowFb(w, h) {
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
-  return window.open("https://www.facebook.com/sharer/sharer.php?u="+ window.location.href, "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  return window.open("https://www.facebook.com/sharer/sharer.php?u="+ window.location.href, "", 
+  	'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 }
 
 function popupwindowTwitter(w, h) {
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
-  return window.open("https://twitter.com/intent/tweet?text=Hey,%20check%20out%20this%20event%20right%20here!&url="+ window.location.href +"&hashtags=HappeNUS", "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  return window.open("https://twitter.com/intent/tweet?text=Hey,%20check%20out%20this%20event%20right%20here!&url="+ window.location.href +"&hashtags=HappeNUS", "", 
+  	'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 }
 
 function popupwindowEmail(w, h) {
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
-  return window.open("mailto:happenus.dev@gmail.com?subject=Hey, check out this event in HappeNUS&body=Click here to go to the event page: "+ window.location.href, "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  return window.open("mailto:happenus.dev@gmail.com?subject=Hey, check out this event in HappeNUS&body=Click here to go to the event page: "+ window.location.href, "", 
+  	'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 }    
