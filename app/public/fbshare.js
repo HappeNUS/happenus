3f10761d1a7c3e917a1671d9c88190a219a2245f
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-08 16:39:15 +0800

    Added link to event and hashtag in twitter box

--- app/public/fbshare.js
+++ app/public/fbshare.js
@@ -7,5 +7,5 @@ function popupwindowFb(w, h) {
 function popupwindowTwitter(w, h) {
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
-  return window.open("https://twitter.com/intent/tweet?text=Hey,%20check%20out%20this%20event%20right%20here!", "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  return window.open("https://twitter.com/intent/tweet?text=Hey,%20check%20out%20this%20event%20right%20here!&url="+ window.location.href +"&hashtags=HappeNUS", "", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
 }  
