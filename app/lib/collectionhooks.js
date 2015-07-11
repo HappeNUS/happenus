dd096e401ae44d125ce0dcb6c6bc36c04198476c
ianngiaw <ian@u.nus.edu>
2015-07-11 18:53:04 +0800

    refactored code

--- app/lib/collectionhooks.js
+++ app/lib/collectionhooks.js
@@ -1,4 +1,4 @@
-Meteor.users.after.insert(function (userId, user) {
+function setDefaultProfileItems (user) {
 	Meteor.users.update({_id: user._id},
 		{$set:
 			{
@@ -14,6 +14,9 @@ Meteor.users.after.insert(function (userId, user) {
 			}
 		}
 	);
+}
+
+function setDisplayName (user) {
 	var displayName;
 	if (user.username) {
 		displayName = user.username;
@@ -21,9 +24,18 @@ Meteor.users.after.insert(function (userId, user) {
 		displayName = user.services.google.name;
 	}
 	Meteor.users.direct.update({_id: user._id}, {$set: {display_name: displayName}});
+}
+
+function sendWelcomeNotif (user) {
 	if (Meteor.isServer) {
 		NotificationFactory.welcomeNotif(user._id);
 	}
+}
+
+Meteor.users.after.insert(function (userId, user) {
+	setDefaultProfileItems(user);
+	setDisplayName(user);
+	sendWelcomeNotif(user);
 });
 
 Events.after.update(function (userId, doc, fieldNames, modifier, options) {
