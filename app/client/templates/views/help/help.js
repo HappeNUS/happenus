6f8a2e07148ffb4384df50596cec7e5c994099ae
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-13 19:10:23 +0800

    Sidebar buttons change color upon click

--- app/client/templates/views/help/help.js
+++ app/client/templates/views/help/help.js
@@ -12,3 +12,46 @@ Template.help.onRendered(function () {
 	    }
 	});
 });
+
+Template.help.events({
+	'click .side-nav a.1': function(event, template) {
+		$('a').removeClass('active');
+		$('a.1').addClass('active');
+	},
+	'click .side-nav a.2': function(event, template) {
+		$('a').removeClass('active');
+		$('a.2').addClass('active');
+	},
+	'click .side-nav a.3': function(event, template) {
+		$('a').removeClass('active');
+		$('a.3').addClass('active');
+	},
+	'click .side-nav a.4': function(event, template) {
+		$('a').removeClass('active');
+		$('a.4').addClass('active');
+	},
+	'click .side-nav a.5': function(event, template) {
+		$('a').removeClass('active');
+		$('a.5').addClass('active');
+	},
+	'click .side-nav a.6': function(event, template) {
+		$('a').removeClass('active');
+		$('a.6').addClass('active');
+	},
+	'click .side-nav a.7': function(event, template) {
+		$('a').removeClass('active');
+		$('a.7').addClass('active');
+	},
+	'click .side-nav a.8': function(event, template) {
+		$('a').removeClass('active');
+		$('a.8').addClass('active');
+	},
+	'click .side-nav a.9': function(event, template) {
+		$('a').removeClass('active');
+		$('a.9').addClass('active');
+	},
+	'click .side-nav a.10': function(event, template) {
+		$('a').removeClass('active');
+		$('a.10').addClass('active');
+	},
+})
