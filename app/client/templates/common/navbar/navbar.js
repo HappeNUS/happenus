9371c5656c03eaa80131d6946511f8abca949f22
Ian Ngiaw <ian@u.nus.edu>
2015-07-15 01:44:52 +0800

    Removed old search

--- app/client/templates/common/navbar/navbar.js
+++ app/client/templates/common/navbar/navbar.js
@@ -1,31 +1,9 @@
 Template.navbar.onRendered(function () {
 	$('.button-collapse').sideNav();
-
-	$('#search').hide();
-
-	$(document).mouseup(function(event){
-		var target = $('#search');
-		var child1 = $('.searchBar');
-		var child2 = $('.searchBar input');
-    if(!(target.is(event.target) || child1.is(event.target) || child2.is(event.target)) && target.is(":visible")) {
-       target.slideUp(100);
-    } else {
-      return false;
-    }
-  });
 });
 
 Template.navbar.events({
 	'click a': function (event, temp) {
 		$('.button-collapse').sideNav('hide');
-	},
-
-	'click #toggle-search': function (event, template) {
-		var search = template.$('#search');
-		if (search.is(":visible")) {
-			search.slideUp(100);
-		} else {
-			search.slideDown(100);
-		}
 	}
 });
