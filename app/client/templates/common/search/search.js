db1e6848b9f7d28c9a8938f6849b8df7480154b8
Ian Ngiaw <ian@u.nus.edu>
2015-07-15 01:46:08 +0800

    Created search template

---
+++ app/client/templates/common/search/search.js
@@ -0,0 +1,48 @@
+var inputDiv, input, icon, fadeLayer, searchBtn, searchContainer, searchResults;
+
+Template.search.onRendered(function(){
+	searchBtn = this.$('#search-btn');
+	inputDiv = this.$('#search-btn .search-input');
+	input = this.$('#search-btn .search-input input');
+	icon = this.$('#search-btn i.search-icon');
+	fadeLayer = this.$('.search .fade-layer');
+	searchContainer = this.$('.search .search-container');
+	searchResults = this.$('.search .search-container ul.search-results');
+
+	inputDiv.addClass('hide');
+	input.focus(function(){input.select()});
+	$(window).click(function(event){
+		console.log('click');
+		if(searchBtn.hasClass('active')) {
+			closeSearch(event);
+		}
+	})
+})
+
+function openSearch (event, template) {
+	event.stopPropagation();
+	inputDiv.removeClass('hide');
+	searchResults.removeClass('hide');
+	icon.addClass('hide');
+	fadeLayer.addClass('show');
+	searchBtn.addClass('active');
+	searchContainer.addClass('active');
+
+	input.focus();
+}
+
+function closeSearch (event, template) {
+	event.stopPropagation();
+	inputDiv.addClass('hide');
+	searchResults.addClass('hide');
+	icon.removeClass('hide');
+	fadeLayer.removeClass('show');
+	searchBtn.removeClass('active');
+	searchContainer.removeClass('active');
+}
+
+Template.search.events({
+	'click #search-btn': openSearch,
+	'click .fade-layer.show': closeSearch,
+	'click #search-close': closeSearch
+});
