66db6c9fe1b674060e9858d058a9466ed48a7ca3
Ian Ngiaw <ian@u.nus.edu>
2015-07-17 01:41:59 +0800

    Use viewmodel to change view type instead of session variables

--- app/client/templates/views/home/homeTabs/homeTabs.js
+++ app/client/templates/views/home/homeTabs/homeTabs.js
@@ -2,33 +2,15 @@ Template.homeTabs.onRendered(function () {
 	$('ul.tabs').tabs();
 });
 
-/* Sets the view Session variable */
-function setView(view) {
-	Session.set("view", view);
-}
-
-/* Sets the sort Session variable */
-function setSort(sort) {
-	Session.set("sort", sort);
-}
-
-function setLimit() {
-	Session.set("limit", 20);
-}
-
 Template.homeTabs.events({
 	'click #featTab': function(event, template){
 		// When the featured tab is clicked
 		// Sets view to 'featured' and sort to 'popularity'
-		setView('featured');
-		setSort('popularity');
-		setLimit();
+		ViewModel.byId('eventList').changeBoth({type: 'featured'}, 'popularity');
 	},
 	'click #subsTab': function(event, template){
 		// When the subscriptions tab is clicked
 		// Sets view to 'subscriptions' and sort to 'latest'
-		setView('subscriptions');
-		setSort('latest');
-		setLimit();
+		ViewModel.byId('eventList').changeBoth({type: 'subscriptions'}, 'latest');
 	}
 });
