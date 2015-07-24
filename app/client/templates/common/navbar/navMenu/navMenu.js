20da3a8c533ad8ea3db99416baeeab6e6f23bc2b
Muhammad Adam <splitfire_1992@hotmail.com>
2015-07-24 19:09:40 +0800

    navMenu now toggle dropdown

--- app/client/templates/common/navbar/navMenu/navMenu.js
+++ app/client/templates/common/navbar/navMenu/navMenu.js
@@ -1,7 +1,48 @@
+var isDropdownAnimating;
+
+function isDropdownShowing (dropdown) {
+	return dropdown.css('display') === 'block' && dropdown.css('opacity') === '1';
+}
+
+function showDropdown (dropdown) {
+	if (!isDropdownAnimating) {
+		dropdown.css('display', 'block');
+		dropdown.addClass('active');
+		dropdown.stop(true, false).animate({'opacity': '1'}, {
+			duration: 225,
+			start: function(){
+				isDropdownAnimating = true;
+				var ddbtn = dropdown.siblings('a.dd-btn');
+				ddbtn.children('i').html('expand_less');
+			},
+			complete: function(){isDropdownAnimating = false}
+		});
+	}
+}
+
+function hideDropdown (dropdown) {
+	if (!isDropdownAnimating) {
+		dropdown.stop(true, false).animate({'opacity': '0'}, {
+			duration: 225,
+			start: function() {
+				isDropdownAnimating = true;
+				var ddbtn = dropdown.siblings('a.dd-btn');
+				ddbtn.children('i').html('more_vert');
+			},
+			complete: function() {
+				dropdown.css('display', 'none');
+				dropdown.removeClass('active');
+				isDropdownAnimating = false;
+			}
+		});
+	}
+}
+
 Template.navMenu.onRendered(function(){
-	$(".menu-dropdown-btn #dropdown-btn").dropdown({
-		constrain_width: false,
-		belowOrigin: true,
+	isDropdownAnimating = false;
+	$('*').click(function(event){
+		var dropdown = $('#dropdown-list.active');
+		hideDropdown(dropdown);
 	});
 });
 
@@ -11,5 +52,13 @@ Template.navMenu.events({
 	},
 	'click a#openAboutModal': function(event, temp) {
 		ViewModel.byId('aboutWindow').openModal();
+	},
+	'click .dd-btn': function (event, template) {
+		var dropdown = template.$('#dropdown-list');
+		if (isDropdownShowing(dropdown)) {
+			hideDropdown(dropdown);
+		} else {
+			showDropdown(dropdown);
+		}
 	}
 });
